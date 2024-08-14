fb7cb4614e2f5956e215d0e2db63cd6fa72d18ac
Abhishek Sharma <abhishek.sh@disroot.org>
2024-08-14 13:54:24 +0530

    Updated the order of migrations

--- priv/repo/migrations/20240814082316_create_votes.exs
+++ priv/repo/migrations/20240814082316_create_votes.exs
@@ -1,4 +1,3 @@
-
 defmodule Ratemynews.Repo.Migrations.CreateVotes do
   use Ecto.Migration
 
@@ -6,7 +5,7 @@ defmodule Ratemynews.Repo.Migrations.CreateVotes do
     create table(:votes) do
       add :broadcaster_id, references(:broadcasters, on_delete: :delete_all), null: false
       add :user_id, references(:users, on_delete: :delete_all), null: false
-      add :vote_type, :integer, null: false
+      add :vote_type, :string, null: false
 
       timestamps()
     end
