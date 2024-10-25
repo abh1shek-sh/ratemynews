d2afdf66b5c15613b6a65612197981050a9cfce3
Abhishek Sharma <abhishek.sh@disroot.org>
2024-10-25 15:39:34 +0530

    Add dep gen_smtp:
    - Added the dep to use Swoosh.Adapters.AmazonSES

--- mix.exs
+++ mix.exs
@@ -53,6 +53,7 @@ defmodule Ratemynews.MixProject do
        compile: false,
        depth: 1},
       {:swoosh, "~> 1.5"},
+      {:gen_smtp, "~> 1.0"},
       {:finch, "~> 0.13"},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
